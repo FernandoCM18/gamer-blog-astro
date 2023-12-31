c304828d6248581fa6f6566d284ef52892f53b3f
FernandoCM18 <fernando.c.m@live.com>
2023-12-30 16:03:54 -0800

    Add boxShadow style for card element

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				'card': '0px 4px 20px 0px rgba(0, 0, 0, 0.10)'
+			}
+		},
 	},
 	plugins: [
 		require('@tailwindcss/forms'),
